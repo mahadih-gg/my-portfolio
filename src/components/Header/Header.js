a91d61ebd5cb92cc234dd208f430c2b34b14bb47
Mahadi92 <mahadi.hasan.937524.mh@gmail.com>
2021-05-04 00:13:16 +0600

    Projects 60% done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,8 +10,8 @@ const Header = () => {
             {/* <ParticlesBg color="#222831" num={20} type="thick" bg={true} /> */}
             <ParticlesBg color="#F4821F" type="tadpole" bg={true} />
             {/* <ParticlesBg type="cobweb" bg={true} /> */}
-            <div className="header_gritting text-center p-5">
-                <h1>Hi, I'm <span className="color-primary">MAHADI HASAN</span></h1>
+            <div className="header-gritting text-center p-5">
+                <h1>Hi, I'm <span className="color-primary font-secondary">MAHADI HASAN</span></h1>
                 <h1>I'm a Front-End Developer</h1>
                 <a href="#about">
                     <button className="header-btn btn-custom">View My Work <span className="ms-2 header-btn-icon"><FontAwesomeIcon icon={faArrowRight} className="header-btn-icon" /></span> </button>
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,27 +1,47 @@
 import React from 'react';
 import './Projects.css';
-import greenCity from '../../images/green-city.png';
+import greenCity from '../../images/green-city.gif';
 
 const Projects = () => {
+
+
     return (
         <section className="pt-5" id="projects">
             <div className="container pt-5">
                 <h1 className="mb-5 color-primary">&lt;Projects&gt;</h1>
 
-                <h1 className="text-warning">Updateing...</h1>
-                <h1>Animation coming soo.....</h1>
-                <h2>Mind koren na. kaler modde sob ses hoye jabe😁</h2>
+                <div className="row pb-5 pt-3 project">
+                    <div className="col-md-6 px-5 project-img">
+                        <h2 className="color-primary mb-4 font-secondary color-secondary">Project 1</h2>
+                        <img src={greenCity} alt="" />
+                        <span>Live</span>
+                        <span></span>
+                    </div>
 
-                <div className="row py-5">
-                    <div className="col-md-4">
-                        <div className="card bg-dark text-white">
-                            <img src={greenCity} className="card-img" alt="..." />
-                            <div className="card-img-overlay">
-                                <h5 className="card-title">Card title</h5>
-                                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                <p className="card-text">Last updated 3 mins ago</p>
-                            </div>
+                    <div className="col-md-6 pe-5 pt-5 project-description">
+                        <h1 className="d-inline color-primary">GREEN CITY </h1>
+                        <h3 className="d-inline color-primary"> ( A Online Nursery )</h3>
+                        <p className="pt-3">
+                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis alias vitae molestias asperiores illo culpa. Quidem, earum?
+                        </p>
+                        <div className="technologies w-100">
+                            <span className="p-2 me-2 color-primary">React</span>
+                            <span className="p-2 me-2 color-secondary">Javascript</span>
+                            <span className="p-2 me-2 color-primary">Bootstrap 5</span>
+                            <span className="p-2 me-2 color-secondary">HTML</span>
+                            <span className="p-2 me-2 color-primary">CSS</span>
+                            <br /> <br />
+                            <span className="p-2 me-2 mt-4 color-secondary">React Route</span>
+                            <span className="p-2 me-2 color-primary">React Hook Form</span>
+                            <span className="p-2 me-2 mt-4 color-secondary">MongoDB</span>
+                            <span className="p-2 me-2 color-primary">Express</span>
+                            <br /> <br />
+                            <span className="p-2 me-2 mt-4 color-secondary">Dotenv</span>
+                            <span className="p-2 me-2 color-primary">Firebase</span>
+                            <span className="p-2 me-2 mt-4 color-secondary">Axios</span>
+                            <span className="p-2 me-2 color-primary">Toastify</span>
                         </div>
+
                     </div>
                 </div>
 
